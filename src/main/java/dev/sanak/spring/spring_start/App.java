4de9f877bd855111e6ae6b9d39bbccd0d81a154b
Olexii Sanak <sanak.alex@gmail.com>
2018-10-17 19:11:28 +0300

    second commit

---
+++ src/main/java/dev/sanak/spring/spring_start/App.java
@@ -0,0 +1,15 @@
+package dev.sanak.spring.spring_start;
+
+import dev.sanak.spring.spring_start.entity.ModelT1000;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ModelT1000 t1000 = context.getBean("t1000", ModelT1000.class);
+        t1000.dance();
+    }
+}
